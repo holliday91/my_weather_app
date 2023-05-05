a318e5f1c30cc59fc17577c649c4f20654c847cc
holliday91 <j.holliday54@gmail.com>
2023-05-05 10:24:02 -0400

    changed_kelvin_To_ImperialApi

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   const [data, setData] = useState({})
   const [location, setLocation] = useState('')
 
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=78b153fb5e6de02d38be2afd4308e3b0`
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=78b153fb5e6de02d38be2afd4308e3b0`
 
   const searchLocation = (event) => {
     if (event.key === 'Enter') {
